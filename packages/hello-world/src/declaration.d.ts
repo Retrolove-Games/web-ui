f894b1cce3f97cb6d346a7bc7962f2f92cb44485
Konrad Fedorczyk <contact@realhe.ro>
2021-12-15 14:12:41 +0100

    Add working sass oconfiguration

--- packages/hello-world/src/declaration.d.ts
+++ packages/hello-world/src/declaration.d.ts
@@ -3,3 +3,9 @@ declare module '*.css' {
   const content: Record<string, string>;
   export default content;
 }
+
+// declaration.d.ts
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
