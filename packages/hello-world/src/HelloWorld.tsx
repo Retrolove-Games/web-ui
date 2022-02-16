7e471ca51db0b74415a0038287a3e51f182127ce
Konrad Fedorczyk <contact@realhe.ro>
2022-02-16 10:07:47 +0100

    Use stiches instead of scss

--- packages/hello-world/src/HelloWorld.tsx
+++ packages/hello-world/src/HelloWorld.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { styled } from "@stitches/react";
 import css from "./HelloWorld.module.scss";
 
-const HelloWorld = () => <h1 className={css.helloWorld}>Hello, World!</h1>;
+const Button = styled('button', {
+  backgroundColor: 'gainsboro',
+  borderRadius: '9999px',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'lightgray',
+  },
+});
+
+const HelloWorld = () => <h1 className={css.helloWorld}>Hello, World! <Button>Cholibka!</Button></h1>;
 export default HelloWorld;
