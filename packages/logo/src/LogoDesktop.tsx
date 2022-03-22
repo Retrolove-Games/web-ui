ddf3489bcf67d9c0120087c065c3bdcfd63984da
Konrad Fedorczyk <fedek6@gmail.com>
2022-03-22 19:07:14 +0100

    Fix SVG import

--- packages/logo/src/LogoDesktop.tsx
+++ packages/logo/src/LogoDesktop.tsx
@@ -2,4 +2,8 @@ import React from "react";
 import { Logo, LogoProps } from "./Logo";
 import LogoSvg from "./logo-standard.svg";
 
-export const LogoDesktop: React.FC<LogoProps> = (props) => <Logo {...props}>{LogoSvg}</Logo>;
+export const LogoDesktop: React.FC<LogoProps> = (props) => (
+  <Logo {...props}>
+    <LogoSvg />
+  </Logo>
+);
