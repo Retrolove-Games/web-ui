17880a9ace8efc805c8f03a2b0096e0e74004d14
Konrad Fedorczyk <fedek6@gmail.com>
2022-03-24 19:37:11 +0100

    Add new tokens

--- packages/themes/src/theme.ts
+++ packages/themes/src/theme.ts
@@ -18,6 +18,19 @@ export const {
     fonts: {
       primary: `'Source Sans Pro', sans-serif`,
       secondary: `'Zilla Slab Highlight', cursive`,
+    },
+    fontSizes: {
+      desktopSmall: "1.04rem",
+      desktopDefault: "1.25rem"
+    },
+    lineHeights: {
+      lh: "1em",
+      lh12: "1.2em",
+      lh13: "1.3em",
+      lh145: "1.45em",
+      lh15: "1.5em",
+      lh1618: "1.618em",
+      lh1842: "1.842em",
     }
   },
 });
