eb8f5c510034d3e5e4f246a0406cae464b506d23
Konrad Fedorczyk <fedek6@gmail.com>
2022-03-24 11:50:08 +0100

    Add color variant to button

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -1,13 +1,26 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable arrow-body-style */
 import React, { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
-import { styled, darkTheme } from "@retrolove-games/ui-themes";
+import { styled } from "@retrolove-games/ui-themes";
 import { VariantProps } from "@stitches/react";
 
 const Wrapper = styled("button", {
   display: "inline-block",
+
+  // Variants
+  variants: {
+    color: {
+      red: {
+        backgroundColor: "$redDefault"
+      },
+      neonBlue: {
+        backgroundColor: "$neonBlueDefault"
+      }
+    }
+  }
 });
 
+export type ComponentVariants = VariantProps<typeof Wrapper>;
 type ButtonProps<T extends ElementType> = {
   as?: T;
   children: ReactNode;
@@ -16,12 +29,13 @@ type ButtonProps<T extends ElementType> = {
 export const Button = <T extends ElementType = "button">({
   as,
   children,
+  color = "neonBlue",
   ...props
-}: ButtonProps<T> & ComponentPropsWithoutRef<T>) => {
+}: ButtonProps<T> & ComponentPropsWithoutRef<T> & ComponentVariants) => {
   const component = as || "button";
 
   return (
-    <Wrapper as={component} {...props}>
+    <Wrapper as={component} color={color} {...props}>
       {children}
     </Wrapper>
   );
