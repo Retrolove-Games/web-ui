8c41c40031fd39ba711c09fe4fa18ea04edb43a8
Konrad Fedorczyk <fedek6@gmail.com>
2022-04-02 16:50:27 +0200

    Change icon typings

--- packages/icon/src/IconMoon.tsx
+++ packages/icon/src/IconMoon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Icon, ComponentVariants } from "./Icon";
 import IconSvg from "./assets/icon-moon.svg";
 
-export const IconMoon: React.VoidFunctionComponent<ComponentVariants> = (props) => (
+export const IconMoon: React.FC<ComponentVariants> = (props) => (
   <Icon {...props}>
     <IconSvg />
   </Icon>
