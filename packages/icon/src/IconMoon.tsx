9f6c4ea8fcf25429d64062e9764e3790ee54a457
Konrad Fedorczyk <fedek6@gmail.com>
2022-04-02 16:52:59 +0200

    Update icon types

--- packages/icon/src/IconMoon.tsx
+++ packages/icon/src/IconMoon.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Icon, ComponentVariants } from "./Icon";
+import { Icon, ComponentType } from "./Icon";
 import IconSvg from "./assets/icon-moon.svg";
 
-export const IconMoon: React.FC<ComponentVariants> = (props) => (
+export const IconMoon: ComponentType = (props) => (
   <Icon {...props}>
     <IconSvg />
   </Icon>
