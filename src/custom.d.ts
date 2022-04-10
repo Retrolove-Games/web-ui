dcd58f9a9ef8695dfaf4819d290dd6ac78e0baf5
Konrad Fedorczyk <fedek6@gmail.com>
2022-04-10 19:40:27 +0200

    Add mouse events to types

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,3 +2,7 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+type MouseEvents = {
+  onClick?: React.MouseEventHandler;
+}
