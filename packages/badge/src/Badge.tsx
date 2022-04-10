d04c79dcd250be43e97bc3c933e8766eddb1963a
Konrad Fedorczyk <fedek6@gmail.com>
2022-04-10 19:39:56 +0200

    Add min value to badge

--- packages/badge/src/Badge.tsx
+++ packages/badge/src/Badge.tsx
@@ -5,6 +5,7 @@ import { Wrapper } from "./styles";
 type ComponentProps = {
   children: number;
   label: string;
+  minValue?: number;
 };
 
 export type ComponentVariants = VariantProps<typeof Wrapper>;
@@ -15,14 +16,24 @@ export type ComponentType = React.VoidFunctionComponent<
 export const Badge: ComponentType = ({
   children,
   size = "small",
+  minValue = 0,
   label,
   ...props
 }) => {
   const [animate, setAnimate] = useState(false);
+  const [displayValue, setDisplayValue] = useState(String(children));
 
   useEffect(() => {
+    if (children < minValue) {
+      setDisplayValue(String(minValue));
+    } else if (children > 9) {
+      setDisplayValue("9+");
+    } else {
+      setDisplayValue(String(children));
+    }
+
     setAnimate(true);
-  }, [children]);
+  }, [children, minValue]);
 
   return (
     <Wrapper
@@ -33,7 +44,7 @@ export const Badge: ComponentType = ({
       onAnimationEnd={() => setAnimate(false)}
       {...props}
     >
-      <span>{children > 9 ? `9+` : children}</span>
+      <span>{displayValue}</span>
     </Wrapper>
   );
 };
