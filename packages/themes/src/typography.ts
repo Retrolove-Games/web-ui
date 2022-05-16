9746587aed356ae40409ddecbc5e587b7e2ffa8b
Konrad Fedorczyk <fedek6@gmail.com>
2022-05-16 13:03:17 +0200

    Add all typography tokens

--- packages/themes/src/typography.ts
+++ packages/themes/src/typography.ts
@@ -1,7 +1,7 @@
 export const fonts = {
   body: `'Source Sans Pro', sans-serif`,
   display: `'Zilla Slab Highlight', cursive`,
-}
+};
 
 export const lineHeights = {
   lh: "1em",
@@ -11,14 +11,40 @@ export const lineHeights = {
   lh15: "1.5em",
   lh1618: "1.618em",
   lh1842: "1.842em",
-}
+};
 
 export const fontSizes = {
+  // Mobile
+  mobileXs: "0.84rem",
+  mobileSm: "0.94rem",
+  mobileP: "1.06rem",
+  mobileDefault: "$mobileP",
+  mobileH6: "1.20rem",
+  mobileH5: "1.34rem",
+  mobileH4: "1.51rem",
+  mobileh3: "1.70rem",
+  mobileH2: "1.91rem",
+  mobileH1: "2.15rem",
+  mobileXl: "2.42rem",
+  mobileXxl: "2.73rem",
+  mobileXxxl: "3.07rem",
+
+  // Desktop
   desktopXs: "0.87rem",
   desktopSm: "1.04rem",
-  desktopDefault: "1.25rem",
-}
+  desktopP: "1.25rem",
+  desktopDefault: "$desktopP",
+  desktopH6: "1.5rem",
+  desktopH5: "1.8rem",
+  desktopH4: "2.16rem",
+  desktopH3: "2.59rem",
+  desktopH2: "3.11rem",
+  desktopH1: "3.73rem",
+  desktopXl: "4.48rem",
+  desktopXxl: "5.37rem",
+  desktopXxxl: "6.45rem",
+};
 
 export const fontWeights = {
-  bodyBold: 600
-}
+  bodyBold: 600,
+};
